b4f02826f7046e29fbf1369c6014ae158eba9941
DongNguyen <ducdong.mg@gmail.com>
2022-11-27 09:31:39 +0900

    add show hide component

---
+++ src/components/ButtonShow.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h2 v-show="isShow">this is the element we want to coltrol</h2>
+
+        <button @click="isShow = !isShow" > show / hide</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            isShow : true
+        }
+    },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
